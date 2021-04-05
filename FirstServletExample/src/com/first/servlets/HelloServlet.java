394085bc6ee18f8149c1e61be447bec5259f0ba2
Kiran <kiranreddy.polu@gmail.com>
2021-04-05 08:15:01 +0530

    JIRA-0000 "Changes are saving"

--- FirstServletExample/src/com/first/servlets/HelloServlet.java
+++ FirstServletExample/src/com/first/servlets/HelloServlet.java
@@ -1,18 +1,21 @@
 package com.first.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.GenericServlet;
+import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebServlet;
+
 
 /**
  * Servlet implementation class HelloServlet
  */
 //@WebServlet("/HelloServlet")
-public class HelloServlet extends GenericServlet {
+public class HelloServlet extends GenericServlet{
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -47,6 +50,10 @@ public class HelloServlet extends GenericServlet {
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Inside Service Method..");
+		PrintWriter out=response.getWriter();
+		out.print("<html> <body>");
+		out.print("<b>This is first servlet example. Hope you are learning Servlets..<b>");
+		out.print("</body></html>");
 	}
 
 }
